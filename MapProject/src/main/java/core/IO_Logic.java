a6b36256614a7170925ee621ad2c37de800cfff0
Michael Hrcek <mhrcek@imsa.edu>
2015-05-07 05:31:38 -0500

    Added in save/load map feature. Currently throws exceptions

--- MapProject/src/main/java/core/IO_Logic.java
+++ MapProject/src/main/java/core/IO_Logic.java
@@ -8,7 +8,13 @@ package core;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
@@ -18,8 +24,54 @@ import javax.swing.ImageIcon;
  */
 public class IO_Logic {
 
-    public static Image loadImage(String path) throws IOException{
+    public static Image loadImage(String path) throws IOException {
         return ImageIO.read(new File(path));
     }
+
+    public static Map loadMap(String path){
+        FileInputStream inStream = null;
+        ObjectInputStream objStream = null;
+        
+        try{
+            
+            inStream = new FileInputStream(path);
+            objStream = new ObjectInputStream(inStream);
+            
+            return (Map) objStream.readObject();
+            
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(IO_Logic.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            try{
+                objStream.close();
+                inStream.close();
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        
+        return null;
+    }
     
+    public static void saveMap(String path, Map map) {
+        ObjectOutputStream objStream = null;
+        FileOutputStream fileStream = null;
+        try {
+            fileStream = new FileOutputStream(path);
+            objStream = new ObjectOutputStream(fileStream);
+            objStream.writeObject(map);
+        } catch (IOException ex) {
+            Logger.getLogger(IO_Logic.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                objStream.close();
+                fileStream.close();
+            } catch (IOException ex) {
+                Logger.getLogger(IO_Logic.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
 }
